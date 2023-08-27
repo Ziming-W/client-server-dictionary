0bcb9f1158592dcbf13be142c158dc88de96d375
Ziming-W <michaelwang2002@hotmail.com>
2023-08-27 17:10:42 +1000

    handcrafted thread pool

---
+++ src/HandcraftedThreading/HandcraftedThreadPool.java
@@ -0,0 +1,43 @@
+package HandcraftedThreading;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.PriorityBlockingQueue;
+
+public class HandcraftedThreadPool {
+    private BlockingQueue<Runnable> taskQueue;
+    private WorkerThread[] workerPool;
+    private final int poolSize;
+
+    public HandcraftedThreadPool(int poolSize) {
+        this.poolSize = poolSize;
+        this.workerPool = new WorkerThread[poolSize];
+        this.taskQueue = new LinkedBlockingQueue<>();
+        for(int i = 0; i < poolSize; i++){
+            workerPool[i] = new WorkerThread(taskQueue);
+            new Thread(workerPool[i]).start();
+        }
+    }
+
+    /**
+     * submit a runnable task, put it into the task queue
+     * @param task runnable task
+     */
+    public void submitTask(Runnable task){
+        try{
+            taskQueue.put(task);
+        }
+        catch (InterruptedException e){
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    /**
+     * shutdown all threads in the workerpool
+     */
+    public void shutdownAll(){
+        for(WorkerThread workerthread:workerPool){
+            workerthread.interrupt();
+        }
+    }
+}
